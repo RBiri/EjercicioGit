9d4d59ca62fc6caec13dc6f217813b15f7a6675f
RBiri <rubengapa97@gmail.com>
2023-10-31 13:28:47 +0100

    tercera interaccion, con documentacion de funcion

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 let nombre = "Ruben";
 let mensaje = "Bienvenid@ a Tau, la casa del Terror"
 
-
+/**
+ * Funcion mensaje de Bienvenid@
+ * Esta funcion recibe dos parametros y añade un elemento h1
+ * con un texto formado por los parametros proporcionados
+ * @author Biri
+ * @version 3
+ * @since 31/10/2023
+ * @param {String} nombre 
+ * @param {String} txt 
+ */
 function mensajeBienvenida(nombre, txt){
     let titulo = document.createElement("h1");
     titulo.innerText= mensaje+", "+nombre;
